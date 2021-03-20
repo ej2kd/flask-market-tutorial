019ac28a175d26336a097ab9b58d68e1f0c1cd0c
ej2kd <eiji.tsukada9415@gmail.com>
2021-03-20 12:30:28 +0900

    implement user authentication

--- market/__init__.py
+++ market/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 # Instantiate flask object
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = 'e3d853eac44f510167d6d586'
 db = SQLAlchemy(app)
-
+bcrypt = Bcrypt(app)
 from market import routes
